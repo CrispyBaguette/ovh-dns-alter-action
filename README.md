244c9cebba8d7ab6c344296d582567de08cf8a17
Alexandre BRUYANT <alexandre.bruyant.external-provider@kuhn.com>
2021-08-09 13:23:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# OVH alter DNS record
+
+This action updates an existing DNS record using the OVH API.
+
+## Inputs
+
+## `application-key`
+
+**Required** Application key.
+
+## `application-secret`
+
+**Required** Application secret.
+
+## `consumer-key`
+
+**Required** Consumer key.
+
+## `api-endpoint`
+
+API endpoint. Defaults to `ovh-eu`.
+
+## `dns-zone`
+
+**Required** DNS zone.
+
+## `record-id`
+
+**Required** DNS record id.
+
+## `subdomain`
+
+Subdomain.
+
+## `target`
+
+DNS record target.
+
+## `ttl`
+
+DNS record TTL.
+
+## Example usage
+
+```yaml
+uses: actions/ovh-dns-alter-action@v1
+with:
+  application-key: foo
+  application-secret: bar
+  consumer-key: far
+  api-endpoint: ovh-us
+  dns-zone: example.com
+  record-id: 42
+  target: "new_record_value"
+```
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ovh/go-ovh/ovh"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// Setup configuration with viper
+	viper.SetDefault("API_ENDPOINT", "ovh-eu")
+	viper.SetEnvPrefix("ODUA")
+	viper.BindEnv("API_ENDPOINT")
+	viper.BindEnv("APPLICATION_KEY")
+	viper.BindEnv("APPLICATION_SECRET")
+	viper.BindEnv("CONSUMER_KEY")
+	viper.BindEnv("DNS_ZONE")
+	viper.BindEnv("RECORD_ID")
+	viper.BindEnv("SUBDOMAIN")
+	viper.BindEnv("TARGET")
+	viper.BindEnv("TTL")
+
+	apiEndpoint := viper.GetString("API_ENDPOINT")
+	appKey := viper.GetString("APPLICATION_KEY")
+	appSecret := viper.GetString("APPLICATION_SECRET")
+	consumerKey := viper.GetString("CONSUMER_KEY")
+
+	client, err := ovh.NewClient(
+		apiEndpoint,
+		appKey,
+		appSecret,
+		consumerKey,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	type Params struct {
+		SubDomain string `json:"subDomain,omitempty"`
+		Target    string `json:"target,omitempty"`
+		TTL       int64  `json:"ttl,omitempty"`
+	}
+
+	var reqBody = &Params{
+		SubDomain: viper.GetString("SUBDOMAIN"),
+		Target:    viper.GetString("TARGET"),
+		TTL:       viper.GetInt64("TTL"),
+	}
+
+	dnsZone := viper.GetString("DNS_ZONE")
+	recordId := viper.GetString("RECORD_ID")
+	query := fmt.Sprintf("/domain/zone/%v/record/%v", dnsZone, recordId)
+
+	if err = client.Put(query, reqBody, nil); err != nil {
+		panic(err)
+	}
+}
